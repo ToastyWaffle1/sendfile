bbf3ee9bd541b1a033d1d602eeb8424bf6d442b6
Owen Conlon <41126855+ToastyWaffle1@users.noreply.github.com>
2020-11-12 14:03:12 -0600

    Update sen.py

--- sen.py
+++ sen.py
@@ -4,7 +4,7 @@ import os
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #specify ip addies here
 host = input('enter the host ip')
 
@@ -12,13 +12,23 @@ port = input('enter port')
 filename = input('enter the name of the file you want to send: ')
 filesize = os.path.getsize(filename)
 print(filesize)
-s = socket.socket
+s = socket.socket()
 	
-s.connect((host, port))
+s.connect((host, int(port)))
 print('You are connecting to '+host+port)
 print('connected')
 s.send('filename+SEPARATOR+filesize'.encode())
 
+#tqdm starts here
+bar = tqdm.tqdm(range(filesize), "Sending "+filename".", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename 'rb') as f:
+	for _ in progress:
+		bytes_read = f.read(BUFFER_SIZE)
+		if not bytes_read:
+			break
+		s.sendall(bytes_read)
+		progress.update(len(bytes_read))
+s.close()
 #create the socket
 '''
 try:
