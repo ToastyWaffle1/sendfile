6ca6ec178c49d12a99b5ef1bf9ef69508fbf4b33
Owen Conlon <41126855+ToastyWaffle1@users.noreply.github.com>
2020-11-12 15:04:25 -0600

    Update senserver.py

--- senserver.py
+++ senserver.py
@@ -7,10 +7,10 @@ SERVER_PORT = 5001
 BUFFER_SIZE = 4096
 SEPARATOR = '<SEPARATOR>'
 
-s = socke.socket()
+s = socket.socket()
 s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
-print('listening as'+SERVER_HOST+SERVER_PORT)
+#print('listening as'+SERVER_HOST+int(SERVER_PORT))
 client_socket, address = s.accept()
 print(''+address+'is now connected')
 received = client_socket.recv(BUFFER_SIZE).decode()
@@ -18,14 +18,14 @@ filename, filesize= received.split(SEPARATOR)
 filename = os.path.basename(filename)
 filesize = int(filesize)
 # tqdm
-bar = tqdm.tqdm(range(filesize), "Sending "+filename".", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename 'wb') as f:
-	for _ in progress:
+bar = tqdm.tqdm(range(filesize), "Sending "+filename, unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, 'wb') as f:
+	for _ in bar:
 		bytes_read = client_socket.recv(BUFFER_SIZE)
 		if not bytes_read:
 			break
 		f.write(bytes_read)
 		#s.sendall(bytes_read)
-		progress.update(len(bytes_read))
+		bar.update(len(bytes_read))
 client_socket.close()
 s.close()
