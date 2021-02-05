405954d32df84f6b22542cd980bf55c9437ca3cf
Owen Conlon <41126855+ToastyWaffle1@users.noreply.github.com>
2021-02-05 14:45:04 -0600

    Update filesend.py

--- filesend.py
+++ filesend.py
@@ -1,6 +1,8 @@
 from socket import *
 import tkinter
+import os
 from threading import *
+#s = socket.socket()
 def receive():
 	while True:
 		try:
@@ -15,7 +17,26 @@ def send(event=None):
 	if msg == "{quit}":
 		client.socket.close()
 		top.quit()
-		'''
+def fileupload():
+	os.system('python sen.py')
+def receivefile():
+	os.system('python senserver.py')
+
+#def UploadAction(event=None):
+ #   filename = filedialog.askopenfilename()
+  #  print('Selected:', filename)
+
+#root = tkinter.Tk()
+#button = tkinter.Button(root, text='Open', command=UploadAction)
+#button.pack()
+'''
+def sendfile(event=None):
+	filename = input('enter the name of the file you want to send: ')
+	filesize = os.path.getsize(filename)
+	print(filesize)
+	s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+		
 def send_file(event=None):
 	msg = my_msg.get()
 	my_msg.set('')
@@ -47,6 +68,15 @@ entry_field.bind("<Return>", send)
 entry_field.pack()
 send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack()
+file_button = tkinter.Button(top, text="Send File", command=fileupload)
+file_button.pack()
+receive_button = tkinter.Button(top, text="receive file", command=receivefile)
+receive_button.pack()
+#root = tkinter.Tk()
+#button = tkinter.Button(root, text='Open', command=UploadAction)
+#button.pack()
+#file_button = tkinter.Button(top, text="file", command=sendfile)
+#file_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
@@ -54,7 +84,7 @@ HOST = input('enter the HOST ip')
 
 PORT = input('enter PORT')
 if not PORT:
-	PORT = 33000
+	PORT = 5001
 else: 
 	PORT = int(PORT)
 BUFSIZ = 4096
